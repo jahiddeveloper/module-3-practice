54b6d442d187ce24f3eeb3393301f3eb077bea62
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-09-19 09:22:37 +0600

    commit-6

--- src/LikeAneDislike.jsx
+++ src/LikeAneDislike.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsUp, faThumbsDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function LikeAndDislike() {
   const [likeCount, setLikeCount] = useState(0);
@@ -20,16 +19,20 @@ export default function LikeAndDislike() {
   const style = {
     marginBottom: "30px",
     padding: "10px 20px",
-  }
+  };
 
   const btn2 = {
     marginLeft: "20px",
-  }
+  };
 
   return (
     <div style={style}>
-      <button onClick={addLike}>{likeCount} <FontAwesomeIcon icon={faThumbsUp} /></button>
-      <button style={btn2} onClick={addDislike}>{disLikeCount} <FontAwesomeIcon icon={faThumbsDown} /></button>
+      <button onClick={addLike}>
+        {likeCount} <FontAwesomeIcon icon={faThumbsUp} />
+      </button>
+      <button style={btn2} onClick={addDislike}>
+        {disLikeCount} <FontAwesomeIcon icon={faThumbsDown} />
+      </button>
     </div>
   );
 }
